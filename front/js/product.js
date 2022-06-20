06d869314214f5215a7fad2644eb03ce32732090
mzerrouk <meidine59@gmail.com>
2022-06-20 14:38:16 +0200

    Script and product are done

--- front/js/product.js
+++ front/js/product.js
@@ -1,3 +1,35 @@
-let pageUrl = window.location.href.toString()
-let searchParams = new URLSearchParams(pageUrl)
-console.log(searchParams.get('id'))
+let url = 'http://localhost:3000/api/products/'
+
+let pageUrl = window.location.href
+let searchParams = new URLSearchParams(new URL(pageUrl).search)
+let id = searchParams.get('id');
+
+fetch(url+id)
+.then((resp) => resp.json())
+.then(function (data){
+    let select = document.getElementById('colors')
+    let divImg = document.getElementsByClassName('item__img')
+    let img = document.createElement('img')
+    let title = document.getElementById('title')
+    let price = document.getElementById('price')
+    let description = document.getElementById('description')
+    img.src = data['imageUrl']
+    img.alt = data['altTxt']
+    divImg[0].appendChild(img)
+    title.textContent = data['name']
+    price.textContent = data['price']
+    description.textContent = data['description']
+    for (let i = 0; i < data['colors'].length; i++) {
+        select.options[select.options.length] = new Option(data['colors'][i],data['colors'][i]);
+    }
+})
+.catch(function(error) {
+  console.log(error);
+});
+
+const button = document.getElementById('addToCart')
+button.addEventListener("click", function() {
+  let img = document.getElementById('img')
+  let title = document.getElementById('title')
+  let price = document.getElementById('price')
+})
